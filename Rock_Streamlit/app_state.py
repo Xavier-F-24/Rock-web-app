94e9a4ed507ce9372d1901946967b5ed48bb1cca
Xavier Fross <popcanpopcan24@gmail.com>
2026-07-09 05:17:22 -0600

    repo root fix not put in , so now is added

--- Rock_Streamlit/app_state.py
+++ Rock_Streamlit/app_state.py
@@ -6,6 +6,14 @@ from typing import TYPE_CHECKING
 
 import streamlit as st
 
+from pathlib import Path
+import sys
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
+
 from rockgame_ui.game_controller import GameStartSettings, start_new_game
 
 if TYPE_CHECKING:
